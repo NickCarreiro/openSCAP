41f231e4f57f5d2a76d20c462f709ee2b45de4c0
NickCarreiro <135732586+NickCarreiro@users.noreply.github.com>
2025-07-15 12:34:37 -0400

    Update scan_disa_stig.sh

--- disa/scan_disa_stig.sh
+++ disa/scan_disa_stig.sh
@@ -5,6 +5,8 @@ SCAP_FILE="./disa/content/U_CAN_Ubuntu_22-04_LTS_V2R2_STIG_SCAP_1-3_Benchmark.xm
 PROFILE="xccdf_mil.disa.stig_profile_MAC-3_Public"
 TIMESTAMP=$(date +%F_%H-%M-%S)
 
+mkdir reports
+
 oscap xccdf eval \
   --profile "$PROFILE" \
   --results "reports/disa_results_$TIMESTAMP.xml" \
